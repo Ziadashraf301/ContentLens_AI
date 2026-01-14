fb5cb64e1ce864b40062fa0524146d46620ddd7e
Ziadashraf301 <ziadashraf98765@gmail.com>
2026-01-14 13:34:39 +0200

    fix(tracing): Update Langfuse to monitor and trace

--- backend/app/core/langfuse.py
+++ backend/app/core/langfuse.py
@@ -1,6 +1,16 @@
 from langfuse.langchain import CallbackHandler
 from .config import settings
+from langfuse import get_client
 
+langfuse = get_client()
+
+# Verify connection
+if langfuse.auth_check():
+    print("Langfuse client is authenticated and ready!")
+else:
+    print("Authentication failed. Please check your credentials and host.")
+
+    
 def get_langfuse_callback():
     """
     Returns a callback handler for LangChain/LangGraph.

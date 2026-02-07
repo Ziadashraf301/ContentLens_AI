264507e5250a94cdc5621da4d73899ff2b5e6f65
Ziadashraf301 <ziadashraf98765@gmail.com>
2026-02-07 23:01:33 +0200

    feat: add frontend chat

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -6,7 +6,7 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import ChatMessage from '../components/ChatMessage';
 import ChatInput from '../components/ChatInput';
-import Loader from '../components/Loader';
+import { Loader } from '../components/Loader';
 import {
   ChatMessage as ChatMessageType,
   ChatSession,
